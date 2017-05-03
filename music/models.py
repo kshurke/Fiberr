620d3a493b0d7bf949d3d67447e07634fa465879
kshurke <kshurke@gmail.com>
2017-05-03 18:52:24 -0500

    VS github test

--- music/models.py
+++ music/models.py
@@ -13,7 +13,7 @@ class Album(models.Model):
 
     def __str__(self):
         return self.album_title+ ' - '+self.artist
-
+#song model
 class Song(models.Model):
     album = models.ForeignKey(Album,on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
